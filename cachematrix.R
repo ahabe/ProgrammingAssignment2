866bd9ccd1426ebbbf33d8f9e1e010f5c8ff176a
Anton Habegger <anton.habegger@gmail.com>
2017-09-02 17:15:52 +0200

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache handling functions for invertible matrices
 
-## Write a short comment describing this function
-
+## it takes a invertible matrix and returns
+## a special matrix object which has the 
+## capability to cache the result rather than
+## calculate all the time 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(i) inv <<- i
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## It expects the special matrix object 
+## from makeCacheMatrix and calulates
+## it invertible if not already
+## available in the special matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(is.matrix(inv)) {
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
